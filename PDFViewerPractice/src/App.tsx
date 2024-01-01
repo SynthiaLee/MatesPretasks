1efecf5c25415bff0a668490a933158327f71158
SynthiaLee <seanne0801@gmail.com>
2024-01-02 02:33:54 +0900

    react-pdf-viewer 라이브러리 기본 적용 테스트

---
+++ PDFViewerPractice/src/App.tsx
@@ -0,0 +1,10 @@
+import "./App.css";
+import SecondTest from "./SecondTest";
+// import Test from "./Test";
+
+function App() {
+  return <SecondTest />;
+  // <Test />;
+}
+
+export default App;
---
+++ PDFViewerPractice/src/SecondTest.tsx
@@ -0,0 +1,22 @@
+import { Worker } from "@react-pdf-viewer/core";
+import { Viewer } from "@react-pdf-viewer/core";
+import okinawa from "./pdf/okinawa.pdf";
+import "@react-pdf-viewer/core/lib/styles/index.css";
+
+export default function SecondTest() {
+  return (
+    <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          border: "1px solid rgba(0, 0, 0, 0.3)",
+          height: "calc(100vh - 6rem)",
+        }}
+      >
+        <Viewer fileUrl={okinawa} theme={{ theme: "dark" }} />
+      </div>
+    </Worker>
+  );
+}
